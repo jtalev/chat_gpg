13c30a12b6839dc1cb5c176694c01fb44089f3ec
j-talev <j.talev@outlook.com>
2024-12-19 20:45:17 +1100

    add models package

---
+++ models/models.go
@@ -0,0 +1,25 @@
+package models
+
+type Employee struct {
+	ID          int    `json:"id"`
+	EmployeeId  string `json:"employee_id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	IsAdmin     bool   `json:"is_admin"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+type LeaveRequest struct {
+	RequestId  int    `json:"request_id"`
+	EmployeeId string `json:"employee_id"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	Type       string `json:"leave_type"`
+	From       string `json:"from_date"`
+	To         string `json:"to_date"`
+	Note       string `json:"note"`
+	IsApproved bool   `json:"is_approved"`
+}
